7b6b626fa3a02cdf5a4e9c3f6939e6b291778454
SUPAMADAFAKABIGBEE <bigbeeismusic@gmail.com>
2023-11-21 13:38:50 +0800

    Some Music

---
+++ MaingameMusic.cpp
@@ -0,0 +1,8 @@
+//
+//  MaingameMusic.cpp
+//  Project
+//
+//  Created by Richard on 2023/11/21.
+//
+
+#include <stdio.h>
---
+++ MaingameMusic.h
@@ -0,0 +1,12 @@
+//
+//  MaingameMusic.h
+//  Project
+//
+//  Created by Richard on 2023/11/21.
+//
+
+#ifndef MaingameMusic_h
+#define MaingameMusic_h
+
+
+#endif /* MaingameMusic_h */
